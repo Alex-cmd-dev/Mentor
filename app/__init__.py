7f669d5380eb03583cd32e91e65a2d616cf952b4
sendistephen <sendi.stev@gmail.com>
2024-10-14 15:04:50 +0300

    refactor(config): Remove sensitive info from config

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config')
+    app.config.from_object('config.Config')
 
-    # define api routes here..
+    @app.route('/test-db')
+    def test_db_connection():
+        try:
+            db.session.execute('SELECT 1')
+            return 'Connected to database successfully'
+        except Exception as e:
+            return f'Error connecting to database: {str(e)}'
+        
     return app
--- background.py
+++ background.py
@@ -6,6 +6,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI =(f'postgresql://{os.getenv("POSTGRES_USER")}:'
-                             f'{os.getenv("POSTGRES_PASSWORD")}@db:5432/{os.getenv("POSTGRES_DB")}')
+    SQLALCHEMY_DATABASE_URI = (
+        f'postgresql://{os.getenv("POSTGRES_USER")}:'
+        f'{os.getenv("POSTGRES_PASSWORD")}@db:5432/{os.getenv("POSTGRES_DB")}'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
